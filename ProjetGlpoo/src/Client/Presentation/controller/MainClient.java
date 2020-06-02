acfb778a54b06c4d419bf91af8b77ea482af3fba
Quentin MICHEL <quentinmichel99@yahoo.fr>
2020-06-02 22:37:01 +0200

    Creation du Main

--- ProjetGlpoo/src/Client/Presentation/controller/MainClient.java
+++ ProjetGlpoo/src/Client/Presentation/controller/MainClient.java
@@ -2,4 +2,13 @@ package Client.Presentation.controller;
 
 public class MainClient {
 
+	public MainClient() {
+		System.out.println("Ceci est le client");
+	}
+	
+	public static void main(String args[]) {
+		new MainClient();
+	}
+	
 }
+
