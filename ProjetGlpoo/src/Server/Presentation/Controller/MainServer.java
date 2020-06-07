5432c5a229ecf5a4f19839e52f557b57dbb01316
Wassangota <khetim.wassil@gmail.com>
2020-06-07 15:05:46 +0200

    Ajout du mainServer

---
+++ ProjetGlpoo/src/Server/Presentation/Controller/MainServer.java
@@ -0,0 +1,13 @@
+package Server.Presentation.Controller;
+
+public class MainServer {
+	
+	public MainServer() {
+		System.out.println("Ceci est un serveur");
+	}
+	
+	public static void main(String args[]) {
+		new MainServer();
+	}
+
+}
